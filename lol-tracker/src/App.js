b16ea25a8b8ca41684c1dc02096885d5d6751398
Abdul Chaudhry <amc864@drexel.edu>
2023-11-15 15:22:33 -0500

    Created all containers for website

--- lol-tracker/src/App.js
+++ lol-tracker/src/App.js
@@ -2,16 +2,6 @@ import {useState} from 'react';
 import axios from 'axios';
 import './App.css';
 
-// Reusable function for handling API responses
-function handleAPIResponse(response, setDataFunction) {
-  setDataFunction(response.data);
-}
-
-// Reusable function for handling API errors
-function handleAPIErrors(error) {
-  console.log(error);
-}
-
 function App() {
   const [searchText, setSearchText] = useState("");
   const[gameList, setGameList] = useState("");
@@ -57,8 +47,8 @@ function App() {
       <div className='side-container'></div>
       <div className="top-container">
       {playerData ? (
-      <> 
-        <div class="image-container">
+      <>
+        <div className ="image-container">
           <img width="100" height="100" src={"http://ddragon.leagueoflegends.com/cdn/11.21.1/img/profileicon/" + playerData.profileIconId + ".png"}></img>
         </div>
         <div class="text-container">
@@ -72,31 +62,60 @@ function App() {
     
       </div>
 
-      <div class="container">
+      <div className="nav-container">
+        {playerData ? (
+          // Render navigation content when playerData is available
+          <>
+            <div className = "nav-bar">
+              <p> Overview | Main Champion Stats</p>
+            </div>
+
+            <div className = "dashboard-bar">
+              <p> Dashboard PlaceHolder</p>
+            </div>
+            {/* Additional content related to playerData */}
+          </>
+        ) : (
+          // Render alternative content when playerData is not available
+          <p></p>
+        )}
+      </div>
+
+  
+
+      <div class="bottom-container">
       <div class="column">
       {rankedData ? (
       <>
         {/* Display Solo/Duo ranked data */}
         {rankedData[0] ? (
-          <div class="box">
+          <div class="ranked-box">
+
             <p>Solo/Duo</p>
             <p>{rankedData[0].tier} {rankedData[0].rank} {rankedData[0].leaguePoints} LP</p>
             <p>{rankedData[0].wins}W {rankedData[0].losses}L {getWinRate(rankedData[0].wins, rankedData[0].losses)}%</p>
+
           </div>
         ) : (
           //Error Handling Output if no solo/duo rank available
+        <div class = "ranked-box">
           <p>Ranked Solo: Unranked </p>
+        </div>
         )}
         {/* Display Flex ranked data */}
         {rankedData[1] ? (
-          <div class="box">
+          <div class="ranked-box">
+
             <p>Flex</p>
             <p>{rankedData[1].tier} {rankedData[1].rank} {rankedData[1].leaguePoints} LP</p>
             <p>{rankedData[1].wins}W {rankedData[1].losses}L {getWinRate(rankedData[1].wins, rankedData[1].losses)}%</p>
+
           </div>
         ) : (
           //Error Handling Output if no flex rank available
+        <div class = "ranked-box">
           <p>Ranked Flex: Unranked</p>
+        </div>
         )}
       </>
       ):(
@@ -105,32 +124,89 @@ function App() {
       </>
       )
       }
+        <div className = "champion-stats-side-container">
+          <p> Champion Sats Placeholder</p>
+        </div>
+
+        <div className = "recently-played-with-container">
+          <p> Recently Played with ____ in Past 20 Games</p>
+        </div>
       </div>
 
       <div class="column">
-      {gameList.length !== 0 ?
-        <>
-          <p>We have data!</p>
-          {
-            gameList.map((gameData, index) =>
-              <>
-                <h2>Match {index + 1}</h2>
-                <div>
-                  {gameData.info.participants.map((data, participantIndex) =>
-                    <p>{data.summonerName}, KDA: {data.kills}/{data.deaths}/{data.assists}</p>)
-                  }
+    {gameList.length !== 0 ? (
+      <>
+        <p>We have data!</p>
+        {gameList.map((gameData, index) => (
+          <div key={index} className="match-summary-box">
+            <h2>Game {index + 1}</h2>
+          <div className = "match-summary-box">
+
+            <div className = "content-container">
+
+              <div className = "group-one">
+                <p> Mode,date,LP,Win/loss, Match Duration</p>
+              </div>
+
+              <div className = "group-two">
+                <p> Summoners champion, level, abilities, and runes</p>
+              </div>
+
+              <div className = "post-stats">
+                <p> K/D/A, KDA, CS, vision score</p>
+              </div>
+              
+              <div className = "group-three">
+                <p> Items bought from shop</p>
+              </div>
+
+              <div className = "group-four">
+                <div className="summoner-icon-name-column">
+                        {gameData.info.participants.slice(0, 5).map((data, participantIndex) => (
+                          <div key={participantIndex} className={`summoner ${searchText === data.summonerName ? "bold" : ""}`}>
+                            <img
+                              className="icon"
+                              src={`https://ddragon.leagueoflegends.com/cdn/13.22.1/img/champion/${data.championName}.png`}
+                              alt={`${data.championName} Icon`}
+                            />
+                            <p>
+                              {data.summonerName}
+                            </p>
+                          </div>
+                        ))}
+                      </div>
+
+                <div className="summoner-icon-name-column">
+                        {gameData.info.participants.slice(5, 10).map((data, participantIndex) => (
+                          <div key={participantIndex} className={`summoner ${searchText === data.summonerName ? "bold" : ""}`}>
+                            <img
+                              className="icon"
+                              src={`https://ddragon.leagueoflegends.com/cdn/13.22.1/img/champion/${data.championName}.png`}
+                              alt={`${data.championName} Icon`}
+                            />
+                            <p>
+                              {data.summonerName} 
+                            </p>
+                          </div>
+                        ))}
                 </div>
-              </>
-            )
-          }
-        </>
-      :
-        <>
-          <p>We have no data!</p>
-        </>
-      }
-      </div>
-    </div>
+
+              </div>
+              <div className = "dropdown-container">
+                <p> V </p>
+              </div>
+
+            </div>
+
+          </div>
+                </div>
+              ))}
+            </>
+          ) : (
+      <p>We have no data!</p>
+    )}
+  </div>
+</div>
     </div>
   );
 
