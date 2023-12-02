fc68e9705502e7d8d6ed32840cf2eed678080b1e
Caleb Liang <ewq5627@gmail.com>
2023-12-01 21:35:58 -0500

    organized App.js into multiple files

--- lol-tracker/src/App.js
+++ lol-tracker/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import './App.css';
 import PlayerInfo from './components/PlayerInfo';
 import Dashboard from './components/Dashboard';
 import RankedInfo from './components/RankedInfo';
