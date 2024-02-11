9b30ee40368303805cb1c15e13e05def3262a469
Abdul Chaudhry <amc864@drexel.edu>
2024-02-11 02:21:30 -0500

    put all large const dictionary data variables into seperate file

--- proxyServer.js
+++ proxyServer.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 var app = express();
 app.use(cors());
 
-const API_KEY = "RGAPI-9f455fac-c0bf-469c-a35e-e304af2dbf50";
+const API_KEY = "RGAPI-b1b2ae57-5ee1-439f-91ae-5935f872dc83";
 
 function getPlayerPUUID(playerName) {
     return axios.get("https://na1.api.riotgames.com" + "/lol/summoner/v4/summoners/by-name/" + playerName + "?api_key=" + API_KEY).then(response => {
